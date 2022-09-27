bb8c3ac76c54bcf80879d6da2dbb67247f821004
Isac Sousa <isacsousadev@gmail.com>
2022-09-27 11:36:42 -0300

    feat: adiciona filter e order em category in memory repository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,3 +1,4 @@
+import { SortDirection } from "../../../@seedwork/repository/repository-contracts";
 import CategoryRepository from "category/domain/repository/category.repository";
 import { InMemorySearchableRepository } from "../../../@seedwork/repository/in-memory.repository";
 import Category from "../../domain/entities/category";
@@ -6,6 +7,7 @@ export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
   implements CategoryRepository.Repository
 {
+  sortableFields: string[] = ["name"];
   protected async applyFilter(
     items: Category[],
     filter: CategoryRepository.Filter
@@ -17,5 +19,19 @@ export default class CategoryInMemoryRepository
     return items.filter((i) => {
       return i.props.name.toLowerCase().includes(filter.toLowerCase());
     });
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Promise<Category[]> {
+    // should retorn the items in the created_at order if sort is empty
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return [...items].sort((a, b) => {
+        return a.props.created_at.getTime() - b.props.created_at.getTime();
+      });
+    }
+    return super.applySort(items, sort, sort_dir);
   }
 }
