7774c47f73af1b58e4c97f18cd0257e36777e4eb
Isac Sousa <isacsousadev@gmail.com>
2022-09-28 08:59:34 -0300

    test: inclui testes para update category usecase

---
+++ src/category/application/usecases/__tests__/update-category.use-case.spec.ts
@@ -0,0 +1,156 @@
+import Category from "../../../domain/entities/category";
+import CategoryInMemoryRepository from "../../../infra/repository/category-in-memory.repository";
+import UpdateCategoryUseCase, { Input } from "../update-category.use-case";
+
+describe("CreateCategoryUseCase Unit Tests", () => {
+  let useCase: UpdateCategoryUseCase;
+  let categoryRepository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    categoryRepository = new CategoryInMemoryRepository();
+    useCase = new UpdateCategoryUseCase(categoryRepository);
+  });
+
+  it("should throw an error if category not found", async () => {
+    const input = {
+      id: "fake_id",
+      name: "New Name",
+    };
+
+    await expect(useCase.execute(input)).rejects.toThrowError(
+      "Entity with id fake_id not found"
+    );
+  });
+
+  it("should update a category", async () => {
+    const spyUpdate = jest.spyOn(categoryRepository, "update");
+    const entity = new Category({ name: "Movie" });
+    categoryRepository["entities"] = [entity];
+    let output = await useCase.execute({ id: entity.id, name: "test" });
+    expect(spyUpdate).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: entity.id,
+      name: "test",
+      description: null,
+      is_active: true,
+      created_at: entity.created_at,
+    });
+
+    type Arrange = {
+      input: {
+        id: string;
+        name: string;
+        description?: null | string;
+        is_active?: boolean;
+      };
+      expected: {
+        id: string;
+        name: string;
+        description: null | string;
+        is_active: boolean;
+        created_at: Date;
+      };
+    };
+    const arrange: Arrange[] = [
+      {
+        input: {
+          id: entity.id,
+          name: "test",
+          description: "some description",
+        },
+        expected: {
+          id: entity.id,
+          name: "test",
+          description: "some description",
+          is_active: true,
+          created_at: entity.created_at,
+        },
+      },
+      {
+        input: {
+          id: entity.id,
+          name: "test",
+        },
+        expected: {
+          id: entity.id,
+          name: "test",
+          description: null,
+          is_active: true,
+          created_at: entity.created_at,
+        },
+      },
+      {
+        input: {
+          id: entity.id,
+          name: "test",
+          is_active: false,
+        },
+        expected: {
+          id: entity.id,
+          name: "test",
+          description: null,
+          is_active: false,
+          created_at: entity.created_at,
+        },
+      },
+      {
+        input: {
+          id: entity.id,
+          name: "test",
+        },
+        expected: {
+          id: entity.id,
+          name: "test",
+          description: null,
+          is_active: false,
+          created_at: entity.created_at,
+        },
+      },
+      {
+        input: {
+          id: entity.id,
+          name: "test",
+          is_active: true,
+        },
+        expected: {
+          id: entity.id,
+          name: "test",
+          description: null,
+          is_active: true,
+          created_at: entity.created_at,
+        },
+      },
+      {
+        input: {
+          id: entity.id,
+          name: "test",
+          description: "some description",
+          is_active: false,
+        },
+        expected: {
+          id: entity.id,
+          name: "test",
+          description: "some description",
+          is_active: false,
+          created_at: entity.created_at,
+        },
+      },
+    ];
+
+    for (const i of arrange) {
+      output = await useCase.execute({
+        id: i.input.id,
+        name: i.input.name,
+        description: i.input.description,
+        is_active: i.input.is_active,
+      });
+      expect(output).toStrictEqual({
+        id: entity.id,
+        name: i.expected.name,
+        description: i.expected.description,
+        is_active: i.expected.is_active,
+        created_at: i.expected.created_at,
+      });
+    }
+  });
+});
